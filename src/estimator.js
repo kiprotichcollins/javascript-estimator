dcfa64f2d781091d8176dac501e7009cdc5f128b
Kip Collo <collo@localhost.localdomain>
2020-04-13 14:01:32 +0300

    made changes to return statement

--- src/estimator.js
+++ src/estimator.js
@@ -4,16 +4,29 @@ import * as newData from './functions';
 
 const impact = {};
 const servereImpact = {};
-const output = [];
 const covid19ImpactEstimator = (data) => {
-    impact.currentlyInfected = newData.currentlyInfected(data.reportedCases);
-    servereImpact.currentlyInfected = newData.serverlyinfected(data.reportedCases);
-    impact.infectionsByRequestedTime = newData.infectionsByRequestedTime(impact.currentlyInfected);
-    servereImpact.infectionsByRequestedTime = newData.infectionsByRequestedTime(
-        servereImpact.currentlyInfected
-    );
+    const input = data;
+    // impact.currentlyInfected = newData.currentlyInfected(data.reportedCases);
+    // servereImpact.currentlyInfected = newData.serverlyinfected(data.reportedCases);
+    // eslint-disable-next-line max-len
+    // impact.infectionsByRequestedTime = newData.infectionsByRequestedTime(impact.currentlyInfected);
+    // servereImpact.infectionsByRequestedTime = newData.infectionsByRequestedTime(
+    //     servereImpact.currentlyInfected
+    // );
 
-    return output.push({ data, impact, output });
+    return {
+        data: input,
+        impact: {
+            currentlyInfected: newData.currentlyInfected(data.reportedCases),
+            infectionsByRequestedTime: newData.infectionsByRequestedTime(impact.currentlyInfected)
+        },
+        servereImpact: {
+            currentlyInfected: newData.serverlyinfected(data.reportedCases),
+            infectionsByRequestedTime: newData.infectionsByRequestedTime(
+                servereImpact.currentlyInfected
+            )
+        }
+    };
 };
 
 export default covid19ImpactEstimator;
